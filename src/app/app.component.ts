e0610510b03d7b8440c295d22d0efd646964dc2d
Raffaele Dodaro <68231732+RaffaeleDodaro@users.noreply.github.com>
2022-01-11 12:07:49 +0100

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+//app.component -> viene richiamato quando abbiamo la index.html e quando chiama l tah html <app-root>
+
+//come funziona un componente in generale? con @Component va a creare un'istanza con:
+//selector che e' il nome del tag html atttraverso il quale possiamo chiamare il componente
+//templateUrl significa "da dove prendo il tag html?"
+//styleUrls modiifichera' la pagina html presente nel templateUrl con nome uguale in templateurl
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,3 +15,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Portfolio';
 }
+//export class  e' la sintassi per creare una classe typescript
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+// app.module -> viene richiamato all'esecuzione del file
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+//da riga 8 a 17 vengono applicati tutti gli app.component
+//se non metto un component li, piuttosto che un modulo, piuttosto che un service 
+// il modulo o altro non funzionera
+
 export class AppModule { }
